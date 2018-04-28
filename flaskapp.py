88f0857d2bc0dea26d88740d0b29f414e4afd647
Ubuntu <ubuntu@ip-172-31-90-110.ec2.internal>
2018-04-28 20:17:08 +0000

    database added

--- flaskapp.py
+++ flaskapp.py
@@ -1,9 +1,13 @@
 from flask import Flask, jsonify
+from flask import request
+from flask import abort
 from threading import Lock
 app = Flask(__name__)
 
 lock = Lock()
 queue = []
+
+import sqlite3
 
 @app.route('/')
 def hello_world():
@@ -13,7 +17,7 @@ def hello_world():
 def put_in_queue(msg):
     with lock:
         queue.append(msg)
-    return jsonify({'resp': "Message was put"})
+    return jsonify({'resp': "Message was put"}), 200
 
 @app.route('/queue', methods=['GET'])
 def get_the_queue():
@@ -27,6 +31,47 @@ def pop_the_queue():
         if len(queue) != 0:
             queue.pop(0)
     return jsonify({'resp': "Message was popped"})
+
+@app.route('/clear', methods=['GET'])
+def clear_queue():
+    with lock:
+        del queue[:]
+    return jsonify({'resp': "Queue was cleared"})
+
+@app.route('/getEvents', methods=['GET'])
+def getEvents():
+    conn = sqlite3.connect('/home/ubuntu/flaskapp/compToIphone.db')
+    c = conn.cursor()
+    resp_list = []
+    for (num, name, reason, icon, time) in c.execute('SELECT * FROM msgToIphone;'):
+        resp_list.append({'name':name, 'reason':reason, 'icon':icon, 'time':time})
+    return jsonify({'resp':resp_list}), 200
+
+@app.route('/clearDB', methods=['PUT'])
+def clearDB():
+    conn = sqlite3.connect('/home/ubuntu/flaskapp/compToIphone.db')
+    c = conn.cursor()
+    c.execute("DELETE FROM msgToIphone;")
+    conn.commit()
+    return jsonify({'resp':"table deleted"}), 200
+
+@app.route('/postEvent', methods=['POST'])
+def eventIncoming():
+    conn = sqlite3.connect('/home/ubuntu/flaskapp/compToIphone.db')
+    c = conn.cursor()
+    d = request.json
+    try:
+        name = d['name']
+        reason = d['reason']
+        icon = d['icon']
+        time = d['time']
+    except:
+        raise ValueError(str(d))
+    c.execute('INSERT INTO msgToIphone (name, reason, icon, time) Values("{}", "{}", "{}", "{}");'.format(name, reason, icon, time))
+    conn.commit()
+    dbc = list(c.execute('SELECT * FROM msgToIphone;'))
+    return jsonify({'resp': dbc}), 200
+
 
 if __name__ == '__main__':
   app.run()
